3cabfaf2c9ff60aefb4f40ec26079d3e422d4a49
Thibaud Lamothe <thibaud.lamothe@gmail.com>
2022-09-10 14:21:55 +0200

    add entrypoint

---
+++ entrypoint.py
@@ -0,0 +1,10 @@
+from src.selenium_sutom import SeleniumSutom
+
+# Définition des chemins
+words_path = "data/mots.txt"
+chromedriver = "./chromedriver"
+
+# Standard
+sutom = SeleniumSutom(driver_path=chromedriver, words_path=words_path)
+sutom.run()
+sutom.close_driver()
